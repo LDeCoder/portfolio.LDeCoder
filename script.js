ad9e57c15767fb97ae4acc4702d7e3298c01d841
LDeCoder <LDeCoder806@gmail.com>
2023-05-18 00:38:04 -0500

    layout and functionality set.

--- script.js
+++ script.js
@@ -27,3 +27,85 @@ for (x = 0; x < collB.length; x++) {
     } 
   });
 }
+
+function myDiv() {
+  var x = document.getElementById("def");
+
+  // console.log("moused over.");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myResp() {
+  var x = document.getElementById("responsibility");
+
+  // console.log("moused over.");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myDev() {
+  var x = document.getElementById("developer");
+
+  // console.log("moused over.");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myAch() {
+  var x = document.getElementById("achiever");
+
+  // console.log("moused over.");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myBel() {
+  var x = document.getElementById("belief");
+
+  // console.log("moused over.");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function myPos() {
+  var x = document.getElementById("positivity");
+
+  // console.log("moused over.");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+// // to avoid the error, the parameter [sendResponse] is necessary!
+// chrome.runtime.onmouseover.addListener(function(msg, sender, sendResponse) {
+//   // do something ...
+
+//   // this line seems meaningless but you have to invoke it to avoid error.
+//   sendResponse({damn: true});
+// });

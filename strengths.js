68bb9c00ff898699ac082ba4f1c477244974a1da
Lori Davis <40526409+LDeCoder@users.noreply.github.com>
2023-06-06 15:59:16 -0500

    Added strengths files via upload
    Links would not work by simply dropping folder into repo.

---
+++ strengths.js
@@ -0,0 +1,58 @@
+myResp();
+myDev();
+myAch();
+myBel();
+myPos();
+
+
+function myResp() {
+  var x = document.getElementById("responsibility");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myDev() {
+  var x = document.getElementById("developer");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myAch() {
+  var x = document.getElementById("achiever");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myBel() {
+  var x = document.getElementById("belief");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function myPos() {
+  var x = document.getElementById("positivity");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+

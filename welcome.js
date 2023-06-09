ccf24e6c54c7fa15e61dcc95d11bb0a64b5246e5
LDeCoder <LDeCoder806@gmail.com>
2023-06-09 04:09:20 -0500

    toggle color schemes functional

--- welcome.js
+++ welcome.js
@@ -1,25 +1,28 @@
-function myFavoriteColor() {
-  var checkBox1 = document.getElementById("color1");
-  var checkBox2 = document.getElementById("color2");
-  var checkBox3 = document.getElementById("color3");
-  var text = document.getElementById("text");
+// function myFavoriteColor() {
+//   var checkBox1 = document.getElementById("color1");
+//   var checkBox2 = document.getElementById("color2");
+//   var checkBox3 = document.getElementById("color3");
+//   var text = document.getElementById("text");
 
-  if (checkBox1.checked == true) {
-    text.style.display = "block";
-    text.style.color = "green";
-    alert("You chose green.");
-  } else if (checkBox2.checked == true) {
-    text.style.display = "block";
-    text.style.color = "purple";
-    alert("You chose purple.");
-  } else if (checkBox3.checked == true) {
-    text.style.display = "block";
-    text.style.color = "blue";
-    alert("You chose blue.");
-  } else {
-    text.style.display = "none";
-  }
-}
+//   if (checkBox1.checked == true) {
+//     text.style.display = "block";
+//     text.style.color = "green";
+//     alert("You chose green.");
+//   } else if (checkBox2.checked == true) {
+//     text.style.display = "block";
+//     text.style.color = "purple";
+//     alert("You chose purple.");
+//   } else if (checkBox3.checked == true) {
+//     text.style.display = "block";
+//     text.style.color = "blue";
+//     alert("You chose blue.");
+//   } else {
+//     text.style.display = "none";
+//   }
+// }
+
+var setTheme = theme => document.documentElement.className = theme;
+
 
 // function myAlert() {
 //   alert("I voted.");
